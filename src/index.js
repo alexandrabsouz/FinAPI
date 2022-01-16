02f1ccbc4e2012cf1657742809dbf9f511a868d8
alexandra <alexandrabsouz@gmail.com>
2022-01-16 12:07:17 -0300

    feat: listing extract

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ app.use(express.json())
 
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body
-    const customerAlreadyExists = customers.some(customer => customer.cpf == cpf)
+    const customerAlreadyExists = customers.some((customer) => customer.cpf == cpf)
 
-    if(customerAlreadyExists == true){
+    if(customerAlreadyExists){
         return response.status(400).json({
             "error": "customer already exists!"
         })
@@ -24,6 +24,16 @@ app.post("/account", (request, response) => {
     })
 
     return response.status(201).send()
+})
+
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params
+    const customer = customers.find((customer) => customer.cpf === cpf)
+
+    return response.status(200).json({
+        "extrato": customer.statement
+    })
 })
 
 
